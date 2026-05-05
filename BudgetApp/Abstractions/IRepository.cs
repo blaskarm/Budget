1d034379ecb64323fa234c65e731b85caddaa38d
blaskarm <emillindhult@gmail.com>
2026-05-05 21:51:52 +0200

    Repository added

---
+++ BudgetApp/Abstractions/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+
+namespace BudgetApp.Abstractions;
+
+public interface IRepository<T> where T : class
+{
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task<bool> DeleteAsync(int id);
+    Task<ObservableCollection<T>> Get();
+    Task<T> GetById(int id);
+}
---
+++ BudgetApp/Data/Repository.cs
@@ -0,0 +1,43 @@
+﻿using BudgetApp.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.ObjectModel;
+
+namespace BudgetApp.Data;
+
+internal sealed class Repository<T> : IRepository<T> where T : class
+{
+    private readonly BudgetDbContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public Repository(BudgetDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public Task<bool> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ObservableCollection<T>> Get()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
